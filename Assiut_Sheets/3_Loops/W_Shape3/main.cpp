1edb82c0567e2f2fb460510fdd383287a5a3239b
Ahmed Hesham <ahmedh246891@gmail.com>
2023-08-27 22:44:24 +0300

    A new problem has been added

---
+++ Assiut_Sheets/3_Loops/W_Shape3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    for(int i=0; i<num ; i++)
+    {
+        for(int z=0; z <num-i-1; z++)
+        {
+            cout<<" ";
+        }
+        for(int j=0; j<2*i+1; j++)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    for(int i=num; i>0 ; i--)
+    {
+        for(int z=0; z <num-i; z++)
+        {
+            cout<<" ";
+        }
+        for(int j=0; j<2*i-1; j++)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}

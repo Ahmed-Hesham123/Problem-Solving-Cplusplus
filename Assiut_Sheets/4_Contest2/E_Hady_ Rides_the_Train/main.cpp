9e08ac90f1e55a209dae0ec97c3b2f538de2e2bc
Ahmed Hesham <ahmedh246891@gmail.com>
2023-09-04 08:45:42 +0300

    A new problem has been added

---
+++ Assiut_Sheets/4_Contest2/E_Hady_ Rides_the_Train/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long seat, row, column;
+    cin >> seat;
+    row = seat / 4;
+    if (row % 2 == 0)
+    {
+        column = seat % 4;
+    }
+    else
+    {
+        column = 3 - (seat % 4);
+    }
+    cout << row << " " << column;
+    return 0;
+}
